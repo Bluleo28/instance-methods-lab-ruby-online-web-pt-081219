bdd33e06ab1113f89ca87db5218a25653e43a637
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-29 02:22:07 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+def Dog
+end
+
+def Dog.new 
+  bark = "woof!"
+end 
+
+def Dog.new 
+  sit = "The dog is sitting"
+  end 
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+def Person
+end
+def 
+ ada ; Person.new 
+ ada = :talk
+ puts  "Hello World!"
+end 
+
+def 
+ada ; Person.new 
+ada = :walk 
+puts "The Person is walking"
+  end
+end 
