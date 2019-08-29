2637663f1a81d35d987f905ed4c6bd23616ef26e
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-29 02:43:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,20 +2,20 @@ def Dog
 end
 
 def 
-  fido = Dog.new 
+  fido ; Dog.new 
   fido = :bark
 end 
   (:puts).with("Woof!")
-fido = Dog.new 
+fido ; Dog.new 
 fido.bark
 end 
 
 def 
-  fido = Dog.new 
+  fido ; Dog.new 
   fido = :sit
 end
   (:puts).with("The dog is sitting")
-  fido = Dog.new 
+  fido ; Dog.new 
   fido.sir
     end 
   end 
